4d762d2a29e3069296bdff25b965c6fac13dcf83
Markus <ma223ik@student.lnu.se>
2015-09-24 11:44:57 +0200

    w9w9w9w9

--- YachtClub/src/boatsNHoes/Main.java
+++ YachtClub/src/boatsNHoes/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("w0w0w0w0w0w0w");
+		System.out.println("w0w0w0w0w0w0w");
+		System.out.println("w0w0w0w0w0w0w");
+		System.out.println("w0w0w0w0w0w0w");
+
 
 	}
 

b6e5c9930d6ee86f8c5a6c744b65ef71e24c92e6
Daniel <Daniel@194.47.122.10>
2015-09-24 11:45:29 +0200

    Det var en gång en murre som var söt och han hade en ganska stor penis.

---
+++ YachtClub/src/boatsNHoes/Member.java
@@ -0,0 +1,8 @@
+package boatsNHoes;
+
+public class Member {
+	
+	String firstName = new String();
+	String lastName = new String();
+
+}
---
+++ YachtClub/src/boatsNHoes/MemberDataHandling.java
@@ -0,0 +1,5 @@
+package boatsNHoes;
+
+public class MemberDataHandling {
+
+}

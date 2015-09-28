ccce372ee036369b25ed3dee770dbec3b57e6aa7
Daniel <Daniel@192.168.0.105>
2015-09-29 01:50:42 +0200

    Wowoowow

--- YachtClub/src/boatsNHoes/Member.java
+++ YachtClub/src/boatsNHoes/Member.java
@@ -2,7 +2,39 @@ package boatsNHoes;
 
 public class Member {
 	
-	String firstName = new String();
-	String lastName = new String();
-
+	private static String name1 = new String();
+	private static String name2 = new String();
+	private static int pn;
+	private static int mid;
+	private static int nBoats;
+	
+	public void member(String firstName, String lastName, int personalNumber, int memberID, int numberOfBoats) {
+		Member.name1 = firstName;
+		Member.name2 = lastName;
+		Member.pn = personalNumber;
+		Member.mid = memberID ;
+		Member.nBoats = numberOfBoats;
+	}
+	
+	public void setMemberFirstName(String name1) {
+		
+	}
+	
+	public void setMemberLastName(String name2) {
+		
+	}
+	
+	public void setMemberPersonalNumber(int pn) {
+		
+	}
+	
+	public void setMemberID(int mid) {
+		
+	}
+	
+	public void setMemberNBoats(int nBoats) {
+		
+	}
+	
+	
 }
--- YachtClub/src/boatsNHoes/MemberDataHandling.java
+++ YachtClub/src/boatsNHoes/MemberDataHandling.java
@@ -1,5 +1,25 @@
 package boatsNHoes;
 
+import java.util.LinkedList;
+
 public class MemberDataHandling {
+	
+	LinkedList members = new LinkedList();
+	
+	public void createMember() {
+		
+	}
+	
+	public void changeMember() {
+		
+	}
+	
+	public void deleteMember() {
+		
+	}
+	
+	public void lookUpMember() {
+		
+	}
 
 }

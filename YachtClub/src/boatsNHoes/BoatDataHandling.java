78f4ca83f05153d9d9f13951892cbe5913ccfeec
Markus <ma223ik@student.lnu.se>
2015-10-06 11:13:38 +0200

    hgf

--- YachtClub/src/boatsNHoes/BoatDataHandling.java
+++ YachtClub/src/boatsNHoes/BoatDataHandling.java
@@ -6,76 +6,62 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class BoatDataHandling {
-	
+	Random rnd = new Random(); //Calling on random in order to generate random numbers for the boatID
+	ArrayList<String> boatArr = new ArrayList<String>();	//the array where the boatID's will be saved
 	Boat boat = new Boat();
-	
-	Random rnd = new Random(); 				//Calling on random in order to generate random numbers for the boatID
-		
-	ArrayList<String> boatArr = new ArrayList<String>();		//the array where the boatID's will be saved
-	
 	String boatID = null;
-	
 	Scanner scan = new Scanner(System.in);
-	
-	
-	public void addNewBoat(String memID){									// this constructur method will be called upon in the Main class in
-																// order to register the boats using their information
-		
+	public void addNewBoat(){	// this constructur method will be called upon in the Main class in 
+		// order to register the boats using their information
 		System.out.println("Set Boat Name");
 		boat.setBoatName(scan.nextLine());
 		System.out.println("Set Boat's Length");
 		boat.setBoatLength(scan.nextLine());
 		System.out.println("Set Boat's Type");
 		boat.setBoatType(scan.nextLine());
-		
-	    
-	}
-	
 
-	public String getBoatID(Random rnd, String boatID)
-	{
-		    boatID = String.valueOf(boat.getBoatName().charAt(0)) +
-	                String.valueOf(boat.getBoatType().charAt(0)) +
-	                Integer.toString(rnd.nextInt(900)+100);
-
-		return boatID;
-		
 	}
-	
+
 
 	public ArrayList registerNewBoat(ArrayList boatArr, String boatID){
 		//add the registered boat to the arraylist
-		
-		
 		boatArr.add(boatID);
-		
 		return boatArr;
 
 	}
-	
-	public String changeBoatData(String boatID){
-		
-	}
-		
-	
-	
-	public String deleteBoat(){
+	public Object changeBoatData(ArrayList boatArr){
+
 		for(int i = 0; i < boatArr.size(); i++)
 		{
-			 if (boatArr.get(i).equals(boatID))
-			 {
-	                boatArr.remove(i);
-	                String x = "the boat has been removed";
-	                return x;
-	            }
-			 
-	         
-	            }
-
-        String y = "Error, Invalid boat ID";
-        return y;
+			if(boatArr.getOwnerID().equals(boatID))
+			{
+				System.out.println("Set a new Boat Name: ");
+				boatArr.get(i).setBoatName(scan.nextLine());
+				System.out.println("Set a new Boat Length");
+				boatArr.get(i).setBoatLenght(scan.nextLine());
+				System.out.println("Set a new Boat Type");
+				boatArr.get(i).setBoatType(scan.nextLine());
+			}
 		}
-		
+		String x = "Error, Invalid boat ID";
+		return x;
 	}
-	
-	
+	public String deleteBoat(ArrayList boatArr){
+		for(int i = 0; i < boatArr.size(); i++)
+		{
+			if (boatArr.get(i).getOwnerID.equals(ownerID))
+			{
+				boatArr.remove(i);
+				String x = "the boat has been removed";
+				return x;
+			}
+
+
+		}
+
+		String y = "Error, Invalid boat ID";
+		return y;
+	}
+}
+
+
